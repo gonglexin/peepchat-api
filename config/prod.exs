4c7e5ccbed9d02e2f970318bdf27a0df0ebbf8fa
Lexin Gong <gonglexin@gmail.com>
2016-04-26 15:25:58 +0800

    Fix typo

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :peepchat, Peepchat.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "serene-escarpment-87569.herokuapp.com", port: 443],
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/manifest.json"
 
 config :peepchat, Peepchat.Repo,
